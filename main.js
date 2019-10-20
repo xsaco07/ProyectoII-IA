25d2b8ac305580caf751b4aa8e638a5e4d221c6b
isaac.mena07 <xsaco07@gmail.com>
2019-10-20 16:06:55 -0600

    Add initial xml parser - wip

--- main.js
+++ main.js
@@ -1,9 +1,48 @@
-var Agent = require("./Agent");
-var ServiceOrder = require("./ServiceOrder");
-var ServiceCodes = require("./ServiceCodes");
+const inputFileField = document.querySelector('input[type=file]');
+inputFileField.addEventListener('change', readFile); // When an input file is loaded
 
-var agent = new Agent("1","Pepe", ServiceCodes['ILA']['code']);
-var order = new ServiceOrder("2","Lucas", ServiceCodes['ILA']['code']);
+function readFile() {
 
-console.log(agent);
-console.log(order);
+  const reader = new FileReader();
+  const inputFile = inputFileField.files[0] // Get the file loaded by the user
+
+  reader.readAsText(inputFile);
+
+  reader.onload = function() { // When the file load is done do something
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(reader.result,"text/xml");
+    var content = xmlDoc.getElementsByTagName("name");
+    for (var i = 0; i < content.length; i++) {
+      console.log(content[i].childNodes[0].nodeValue);
+    }
+    console.log(content);
+    document.getElementById('file-name').innerHTML = inputFile.name;
+  }
+}
+
+class XMLParser {
+
+  static parseAgentFile(xmlContent) {
+    var agentParsed = null;
+    var services = []
+    var agentId = "", name = "";
+
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(xmlContent,"text/xml");
+    var XMLagents = xmlDoc.getElementsByTagName("agent");
+    for (var i = 0; i < XMLagents.length; i++) {
+      agentId = XMLagents[i].getAttribbute('id');
+      name = XMLagents[i].getAttribbute('name');
+      var XMLservices = XMLagents[i].childNodes;
+      for (var i = 0; i < array.length; i++) {
+        array[i]
+      }
+    }
+
+
+  }
+
+  static parseServiceFile(xmlContent) {
+
+  }
+}
