e1a2e1b91008907733092ca03c25869b314a1488
isaac.mena07 <xsaco07@gmail.com>
2019-10-20 20:51:32 -0600

    Improve index structure

--- main.js
+++ main.js
@@ -133,22 +133,62 @@ class XMLParser {
   }
 }
 
-function fileLoaded() {
+// parseFlag = false -> parseAgent
+// parseFlag = true -> parseService
+function fileLoaded(parseFlag) {
 
   const reader = new FileReader();
-  const inputFile = inputFileField.files[0] // Get the file loaded by the user
+  var inputFile = null;
+
+  if (!parseFlag) {
+    inputFile = agentFileField.files[0] // Get the file loaded by the user
+  }
+  else {
+    inputFile = serviceFileField.files[0] // Get the file loaded by the user
+  }
 
   reader.readAsText(inputFile);
 
   reader.onload = function() { // When the file reading is done do something
-    // var agents = XMLParser.parseAgentFile(reader.result);
-    // console.log(agents);
-    var services = XMLParser.parseServiceFile(reader.result);
-    console.log(services);
-    
-    document.getElementById('file-name').innerHTML = inputFile.name;
+
+    if (!parseFlag) {
+      var agents = XMLParser.parseAgentFile(reader.result);
+
+      document.getElementById('agent-file-name').innerHTML = inputFile.name;
+
+      document.getElementById('agent-icon').setAttribute("class", "fas fa-check");
+
+      var hNode = document.createElement("h5");
+      var fileNameNode = document.createTextNode("Archivo subido: " + inputFile.name);
+      hNode.appendChild(fileNameNode);
+      document.getElementById('agent-drop').appendChild(hNode);
+
+      console.log(agents);
+    }
+    else {
+      var services = XMLParser.parseServiceFile(reader.result);
+      document.getElementById('service-file-name').innerHTML = inputFile.name;
+      document.getElementById('service-icon').setAttribute("class", "fas fa-check");
+
+      var hNode = document.createElement("h5");
+      var fileNameNode = document.createTextNode("Archivo subido: " + inputFile.name);
+      hNode.appendChild(fileNameNode);
+      document.getElementById('service-drop').appendChild(hNode);
+
+      console.log(services);
+    }
   }
 }
 
-const inputFileField = document.querySelector('input[type=file]');
-inputFileField.addEventListener('change', fileLoaded); // When an input file is loaded
+//------------------------------------------------------------------------------
+
+const agentFileField = document.getElementById('agent-file-input');
+const serviceFileField = document.getElementById('service-file-input');
+
+agentFileField.addEventListener('change', function(){
+  fileLoaded(false); // When an input agent file is loaded
+});
+
+serviceFileField.addEventListener('change', function() {
+  fileLoaded(true);
+}); // When an input service file is loaded
